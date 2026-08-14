7153988fbbe930630e3498d9bb189ce2132c2487
xreyc <segumareyco@gmail.com>
2026-08-15 01:03:02 +0800

    Copy static assets in Vite build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  publicDir: "static",
   plugins: [svelte()],
   resolve: {
     alias: {
